3617c9588b7bdf02457bcec6a1070ff503396f1f
Idrees Daudzai <idreesdaudzai2000@gmail.com>
2025-05-18 15:20:49 +0430

    Merge branch 'danishjo' into daudzai, resolved conflicts

--- src/main/java/mcit/ddr/innovation/dto/CommitteeDTO.java
+++ src/main/java/mcit/ddr/innovation/dto/CommitteeDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CommitteeDTO {
+
     private String name;
     private String describtion;
     // private Long createdById; // ID of the user creating the committee
