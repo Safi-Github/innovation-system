1990cc29dad9484e0bcd84fc8c53e2344e2b8263
Idrees Daudzai <128482113+daudzai123@users.noreply.github.com>
2025-07-05 12:41:28 +0430

    Update SecurityConfiguration.java

--- src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
+++ src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
@@ -87,7 +87,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // make sure frontend runs here
+        configuration.setAllowedOrigins(List.of("http://10.10.255.63:3000")); // make sure frontend runs here
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
