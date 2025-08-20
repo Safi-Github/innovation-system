464f9d7bf2f0a2a705bb5018842e7a4db1725050
Idrees Daudzai <128482113+daudzai123@users.noreply.github.com>
2025-08-20 10:29:58 +0430

    Update SecurityConfiguration.java

--- src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
+++ src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
@@ -97,7 +97,7 @@ public class SecurityConfiguration {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",          // local dev
                 "http://103.132.98.108:3000",     // React dev server on remote
-                "https://ictinnovation.gov.af:3000"    // production domain
+                "https://ictinnovation.gov.af"    // production domain
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
