bdb109484acc68b11c2314c7304acaaf8b88607e
Idrees Daudzai <128482113+daudzai123@users.noreply.github.com>
2025-09-13 15:25:39 +0430

    Update SecurityConfiguration.java

--- src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
+++ src/main/java/mcit/ddr/innovation/security/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
                     registry.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 
                     // public API endpoints
-                    registry.requestMatchers("/home", "/api/register/**", "/api/authenticate", "/api/verify-email").permitAll();
+                    registry.requestMatchers("/home", "/api/register/**", "/api/authenticate", "/api/users/forgot-password").permitAll();
                     registry.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
 
                     // static files (frontend)
